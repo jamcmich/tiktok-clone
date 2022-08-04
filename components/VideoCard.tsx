a6300757dde4bdc031cb6ed66d19b4956d179fae
Jacob McMichael <jacobmcmichael@gmail.com>
2022-08-04 15:35:44 -0700

    Added logic to VideoCard component for displaying user profile images on posts

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import { Video } from '../types';
+import React, { useState, useEffect, useRef } from 'react';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+
+import { Video } from '../types';
 
 interface IProps {
-    post: Video;
+	post: Video;
 }
 
-const VideoCard: NextPage<IProps> = ({ post }) => {    
-  return (
-    <div>VideoCard</div>
-  )
-}
+const VideoCard: NextPage<IProps> = ({ post }) => {
+	return (
+		<div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+			<div>
+				<div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+					<div className='md:w-16 md:h-16 w-10 h-10'>
+						<Link href=''>
+							<React.Fragment>
+								<Image
+									width={62}
+									height={62}
+									className='rounded-full'
+									src={post.postedBy.image}
+									alt='profile photo'
+									layout='responsive'
+								/>
+							</React.Fragment>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default VideoCard
+export default VideoCard;
