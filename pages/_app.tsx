439f6549cc346f597b2c82212b3df423c4fd590c
Jacob McMichael <jacobmcmichael@gmail.com>
2022-08-04 17:05:02 -0700

    Added example .env file for production; Refreshed Sanity token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { useState, useEffect } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
@@ -16,7 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 	if (isSSR) return null;
 
 	return (
-		<div>
+		<GoogleOAuthProvider clientId=''>
 			<Navbar />
 			<div className='flex gap-6 md:gap-20'>
 				<div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
@@ -27,7 +28,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 					<Component {...pageProps} />
 				</div>
 			</div>
-		</div>
+		</GoogleOAuthProvider>
 	);
 };
 
