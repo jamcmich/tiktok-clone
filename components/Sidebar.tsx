02c450573ac7399bff76165663d78f40350a1a98
Jacob McMichael <jacobmcmichael@gmail.com>
2022-08-09 17:41:11 -0700

    Added comments view to post details page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,12 +8,12 @@ import { ImCancelCircle } from 'react-icons/im';
 import SuggestedAccounts from './SuggestedAccounts';
 import Discover from './Discover';
 import Footer from './Footer';
-// import useAuthStore from '../store/authStore';
+import useAuthStore from '../store/authStore';
 
 const Sidebar: NextPage = () => {
 	const [showSidebar, setShowSidebar] = useState<Boolean>(true);
 	const { pathname } = useRouter();
-	// const { fetchAllUsers, allUsers }: any = useAuthStore();
+	const { fetchAllUsers, allUsers }: any = useAuthStore();
 
 	const activeLink =
 		'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
@@ -49,10 +49,10 @@ const Sidebar: NextPage = () => {
 					</div>
 
 					<Discover />
-					{/* <SuggestedAccounts
+					<SuggestedAccounts
 						fetchAllUsers={fetchAllUsers}
 						allUsers={allUsers}
-					/> */}
+					/>
 					<Footer />
 				</div>
 			)}
