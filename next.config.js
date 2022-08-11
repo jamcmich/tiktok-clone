bbad1267ea61b8ae36688aefa43ea51d803f1e62
Jacob McMichael <jacobmcmichael@gmail.com>
2022-08-10 19:58:21 -0700

    Changed profile images in suggested section to fill their containers; TODO: turn the profile images into a reusable component

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'en.wikipedia.org'],
+		domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'upload.wikimedia.org'],
 	},
 };
 
